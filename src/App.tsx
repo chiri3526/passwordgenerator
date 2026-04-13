0c1ccc7b74239288563b0e15858a4e7200ef10ae
chiri3526 <82017839+chiri3526@users.noreply.github.com>
2026-04-13 14:46:10 +0900

    Update text for clarity in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -353,7 +353,7 @@ function AppScreen({
                   />
                   <div>
                     <strong>紛らわしい文字を除外</strong>
-                    <span>Oと0など、見分けにくい文字を除外します。</span>
+                    <span>見分けにくい文字を除外します。</span>
                   </div>
                 </label>
               </div>
